7b16ebe1a56b1fb36a8dc1c5d658ec41835240d9
Greg Rubino <greg@elephantventures.com>
2017-02-03 17:51:40 -0500

    changed scoring to rows and columns and move() to sub squares

--- solve.py
+++ solve.py
@@ -15,32 +15,32 @@ class SudokuSolve(simanneal.Annealer):
             raise Exception('expected a list')
         self.size = len(hints)
         self.hints = hints
-
-        def __fill(row):
-            missing = [i for i in range(self.size+1) if i not in set(row)]
-            def __replace(e):
-                return e if e is not None else missing.pop()
-            return [__replace(i) for i in row]
-
-        self.state = [__fill(row) for row in hints]
-
+        self.mini_square_size = int(math.sqrt(float(self.size)))
+        self.state = self.hints
         super(SudokuSolve, self).__init__(self.state)
 
+    def __mini_square_set(self):
+        return [(i, j) for i, j in itertools.product(range(0, self.size, self.mini_square_size),
+                                                     range(0, self.size, self.mini_square_size))]
+
+    def __mini_square_hints(self, i, j):
+        return set([self.hints[k][l] for k, l in itertools.product(range(i, i+self.mini_square_size),
+                                                                   range(j, j+self.mini_square_size))
+                    if self.hints[k][l] is not None])
+
     def move(self):
-        self.state = self.hints
-        for i, j in itertools.product(range(self.size), range(self.size)):
-            if self.hints[i][j]:
-                self.state[i][j] = random.choice(range(1,self.size+1))
+        for i, j in self.__mini_square_set():
+            hints = self.__mini_square_hints(i, j)
+            choices = [v for v in range(1, self.size + 1) if v not in hints]
+            random.shuffle(choices)
+            for m, n in itertools.product(range(self.mini_square_size), range(self.mini_square_size)):
+                if self.hints[i+m][j+n] is None:
+                    self.state[i+m][j+n] = choices.pop()
 
     def energy(self):
-        e = 0
+        e = 0.0
+        for row in self.state:
+            e -= len(set(row))
         for col in zip(*self.state):
-            e += (self.size - len(set(col)))
-
-        size_root = int(math.floor(math.sqrt(self.size)))
-        for i in range(0, self.size, size_root):
-            for j in range(0, self.size, size_root):
-                e += (self.size - len(set([self.state[k][l]
-                                           for k, l in itertools.product(range(i, i + size_root),
-                                                                         range(j, j + size_root))])))
+            e -= len(set(col))
         return float(e)
